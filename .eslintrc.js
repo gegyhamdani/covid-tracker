f76f408e3bcf63fd016669cb17d8c138e415f166
Gegy Hamdani <gegyhamdani@gmail.com>
2020-03-18 22:08:49 +0700

    edit eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,27 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:jsx-a11y/recommended",
-    "prettier"
-  ],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
-  },
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
     },
-    ecmaVersion: 2018,
-    sourceType: "module"
-  },
-  plugins: ["react", "jsx-a11y", "prettier", "@typescript-eslint"],
-  rules: {
-    "prettier/prettier": ["error"]
-  }
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
 };
