a108c69018ffa8e2e165380f7fc0a091039a89a4
Gegy Hamdani <gegyhamdani@gmail.com>
2020-03-19 21:52:52 +0700

    add styled component prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,28 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "airbnb",
+    "prettier"
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "airbnb", 
-        "prettier",
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "prettier",
-        "react-hooks"
-    ],
-    "rules": {
-        "prettier/prettier": ["error"],
-    }
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "prettier", "react-hooks", "better-styled-components"],
+  rules: {
+    "prettier/prettier": ["error"],
+    "better-styled-components/sort-declarations-alphabetically": 2
+  }
 };
