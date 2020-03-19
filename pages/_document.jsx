3d13560559d2c53377b6bf71995f90ba7bbc42ab
gegyhamdani <gegyhamdani@gmail.com>
2020-03-19 18:12:36 +0700

    create _document and manifest

---
+++ pages/_document.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <title>Covid19 - Tracker</title>
+
+          <meta name="Covid19" content="Corona Virus, Tracker" />
+          <meta name="robots" content="noindex" />
+          <meta name="AdsBot-Google" content="noindex" />
+
+          <link rel="shortcut icon" href="favicon.ico" />
+          <link rel="apple-touch-icon" href="favicon.ico" />
+          <link rel="manifest" href="manifest.json" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
