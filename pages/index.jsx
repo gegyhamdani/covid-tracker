f1d9a45e52f13e72c46c76e9bda828a22bc68c50
gegyhamdani <gegyhamdani@gmail.com>
2020-03-29 22:36:14 +0700

    create context to provide data

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import useSWR from "swr";
 
 import LoadingScreen from "../src/components/LoadingScreen";
@@ -6,51 +6,45 @@ import Layout from "../src/components/Layout";
 
 import api from "../src/services/api";
 
+import Context from "../src/util/appContext";
+
 const fetcher = url => api.get(url);
 
 const Home = () => {
   const { data, error } = useSWR("/countries/Indonesia", fetcher);
-  const [confirmed, setConfirmed] = useState(0);
-  const [recovered, setRecovered] = useState(0);
-  const [death, setDeath] = useState(0);
-  const [updated, setUpdated] = useState(new Date().toLocaleString());
-
-  useEffect(() => {
-    if (!data) return;
-    const { data: information } = data;
-    setConfirmed(information.confirmed.value);
-    setRecovered(information.recovered.value);
-    setDeath(information.deaths.value);
-    setUpdated(new Date(information.lastUpdate).toLocaleString());
-  }, [data]);
+  const { data: information } = data;
 
   if (error) return <div>failed to load</div>;
   if (!data) return <LoadingScreen />;
 
   return (
     <>
-      <Layout
-        confirmed={confirmed}
-        recovered={recovered}
-        death={death}
-        updated={updated}
-      />
-      <style jsx global>
-        {`
-          html,
-          body {
-            padding: 0;
-            margin: 0;
-            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-              sans-serif;
-          }
+      <Context.Provider
+        value={{
+          confirmed: information.confirmed.value,
+          recovered: information.recovered.value,
+          death: information.deaths.value,
+          updated: new Date(information.lastUpdate).toLocaleString()
+        }}
+      >
+        <Layout />
+        <style jsx global>
+          {`
+            html,
+            body {
+              padding: 0;
+              margin: 0;
+              font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+                Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+                sans-serif;
+            }
 
-          * {
-            box-sizing: border-box;
-          }
-        `}
-      </style>
+            * {
+              box-sizing: border-box;
+            }
+          `}
+        </style>
+      </Context.Provider>
     </>
   );
 };
